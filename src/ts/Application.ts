d6dbde301b8f9185dfcc67c070e85bb3842e9bf7
odango_san <odangosan333@gmail.com>
2016-11-09 00:27:22 +0900

    ブdivide webpack.config browser/electron

--- src/ts/Application.ts
+++ src/ts/Application.ts
@@ -1,8 +1,31 @@
-export namespace App {
-    export class Application {
-        constructor() {
-            console.log("hello world.");
-        }
+import * as Vue from "Vue";
+import { Component } from "vue-typed"
+import * as child_process from "child_process"
+import SofTalk from "./SofTalk"
+import WebspeechApi from "./WebspeechApi"
+import * as io from "socket.io-client";
+
+@Component()
+export default class Application extends Vue {
+    speaker: Speaker;
+    socket = io.connect("http://localhost:3000");
+    constructor() {
+        super();
+        console.log("hello world.");
+        this.speaker = new SofTalk();
+    }
+    message: string = 'Hello!'
+    speak() {
+        this.socket.emit('message', this.message);
+        this.speaker.speak(this.message);
     }
 }
-export default App.Application;
+window.addEventListener("load", () => {
+    var app = new Application();
+    app.$mount("#app");
+    // a.speak("今回もNPC関連は分かりづらいね");
+    // a.speaker = new WebspeechApi();
+    // a.speak("今回もNPC関連は分かりづらいね");
+    // var path = "E:/tools/softalk/SofTalk.exe";
+
+});
--- src/ts/Main.ts
+++ src/ts/Main.ts
@@ -1,2 +1,63 @@
-import Application from "./Application"
-let m = new Application();
+const electron = require('electron');
+const path = require('path')
+// const url = require('url')
+import * as url from "url";
+// const client = require('electron-connect').client;
+const BrowserWindow: typeof Electron.BrowserWindow = electron.BrowserWindow;
+const app: Electron.App = electron.app;
+
+class Main {
+    mainWindow: Electron.BrowserWindow = null;
+    constructor(public app: Electron.App) {
+        this.app.on('ready', this.onReady);
+        this.app.on("window-all-closed", () => {
+            if (process.platform !== 'darwin') {
+                app.quit();
+            }
+        });
+        this.app.on('activate', function () {
+            if (this.mainWindow === null) {
+                this.createWindow();
+            }
+        });
+    }
+
+    onReady() {
+        // Create the browser window.
+        this.mainWindow = new BrowserWindow({ width: 800, height: 600 });
+        this.mainWindow.loadURL(url.format({
+            pathname: path.join(__dirname, "html", 'index.html'),
+            protocol: 'file:',
+            slashes: true
+        }));
+        this.mainWindow.on('closed', () => {
+            this.mainWindow = null;
+        });
+        //   Open the DevTools.
+        this.mainWindow.webContents.openDevTools();
+    }
+}
+
+var Express = require("express")();
+var http = require('http').Server(Express);
+var socketio = require('socket.io')(http);
+Express.get('/', function (req, res) {
+    res.sendFile(__dirname + '/html/browser.html');
+});
+Express.get('/js/browser.bundle.js', function (req, res) {
+    res.sendFile(__dirname + '/js/browser.bundle.js');
+});
+socketio.on('connection', function (socket) {
+    socket.on('message', function (msg) {
+        socketio.emit('message', msg);
+    });
+    // socket.on('message', function (msg) {
+    //     socketio.emit('message', msg);
+    // });
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
+const myapp = new Main(app);
+// client.create(myapp.mainWindow);
